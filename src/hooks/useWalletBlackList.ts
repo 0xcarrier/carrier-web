d1f9537840253105ece4b605354c8fd45d94e05f
duxiaofeng-github <csdxf297951292@gmail.com>
2024-01-11 13:42:11 +0800

    add blacklist test data

--- src/hooks/useWalletBlackList.ts
+++ src/hooks/useWalletBlackList.ts
@@ -1,10 +1,21 @@
+import { CLUSTER } from '../utils/consts';
 import { useData } from './useData';
 
 const BLOCKED_WALLETS = process.env.BLOCKED_WALLETS;
 const TRMLABS_KEY = process.env.TRMLABS_KEY;
 
+export const BLACKLIST_SCREEN_URL =
+  CLUSTER === 'mainnet'
+    ? 'https://api.trmlabs.com/public/v1/sanctions/screening'
+    : CLUSTER === 'testnet'
+    ? process.env.NODE_ENV === 'production'
+      ? '/public/wallet-blacklist-testdata.json'
+      : '/wallet-blacklist-testdata.json'
+    : '';
 export const blockedWallets: string[] =
   BLOCKED_WALLETS && typeof BLOCKED_WALLETS === 'string' ? BLOCKED_WALLETS.split(',') : [];
+
+const memCache: { [walletAddress: string]: boolean } = {};
 
 export function useWalletBlackList(options: { walletAddress?: string }) {
   const { walletAddress } = options;
@@ -16,6 +27,10 @@ export function useWalletBlackList(options: { walletAddress?: string }) {
           return true;
         }
 
+        if (memCache[walletAddress] != null) {
+          return memCache[walletAddress];
+        }
+
         const headers: HeadersInit = {
           'Content-Type': 'application/json',
         };
@@ -24,17 +39,19 @@ export function useWalletBlackList(options: { walletAddress?: string }) {
           headers['Authorization'] = `Basic ${TRMLABS_KEY}`;
         }
 
-        const resp = await fetch(`https://api.trmlabs.com/public/v1/sanctions/screening`, {
+        const resp = await fetch(BLACKLIST_SCREEN_URL, {
           signal,
-          method: 'POST',
+          method: CLUSTER === 'mainnet' ? 'POST' : 'GET',
           headers,
-          body: JSON.stringify([{ address: walletAddress }]),
+          body: CLUSTER === 'mainnet' ? JSON.stringify([{ address: walletAddress }]) : undefined,
         });
-        // DO NOT cache the result, because it can be tampered with and bypass the checks.
+        // DO NOT local cache the result, because it can be tampered with and bypass the checks.
         const respJSON = await resp.json();
         const isSanctioned = Array.isArray(respJSON)
           ? respJSON.some((item) => item.address === walletAddress && item.isSanctioned)
           : false;
+
+        memCache[walletAddress] = isSanctioned;
 
         return isSanctioned;
       }
