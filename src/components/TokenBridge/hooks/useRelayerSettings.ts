06ca3f9d02ed5841570b0bfc859710a6591f2b8a
duxiaofeng-github <csdxf297951292@gmail.com>
2024-01-15 17:26:10 +0800

    fix solana relayer settings

--- src/components/TokenBridge/hooks/useRelayerSettings.ts
+++ src/components/TokenBridge/hooks/useRelayerSettings.ts
@@ -2,6 +2,8 @@ import { useEffect, useMemo, useState } from 'react';
 import { getLocalStorageAdvanceOptions } from '../../../utils/localStorageAdvanceOptions';
 import { CarrierChainId } from '../../../utils/consts';
 import { needTransferByMRL, needTransferByXCM } from '../../../utils/polkadot';
+import { isCarrierEVMChain } from '../../../utils/web3Utils';
+import { CHAIN_ID_SOLANA } from '@certusone/wormhole-sdk';
 
 export function useRelayerSettings(options: { sourceChainId?: CarrierChainId; targetChainId?: CarrierChainId }) {
   const { sourceChainId, targetChainId } = options;
@@ -16,8 +18,13 @@ export function useRelayerSettings(options: { sourceChainId?: CarrierChainId; ta
         // if the source chain or target chain is polkachain, then we enable the relayer by default.
         needTransferByMRL(sourceChainId, targetChainId)
       ) {
-        setIsUsingRelayer(true);
-        setIsRelayerSettingDisabled(false);
+        if (targetChainId === CHAIN_ID_SOLANA) {
+          setIsUsingRelayer(false);
+          setIsRelayerSettingDisabled(true);
+        } else {
+          setIsUsingRelayer(true);
+          setIsRelayerSettingDisabled(false);
+        }
       } else if (needTransferByXCM(sourceChainId, targetChainId)) {
         setIsUsingRelayer(false);
         setIsRelayerSettingDisabled(true);
